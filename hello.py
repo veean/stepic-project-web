bba34d37b0e2a2be6e9a12f815c256bbf5fd3f76
veean <nersesowm@gmail.com>
2016-11-21 21:09:05 +0000

    gunicorn testing app

---
+++ hello.py
@@ -0,0 +1,8 @@
+def app(environ, start_response):
+    data= b"hello world!\n"
+    start_response("200 OK", [
+        ("Content-Type", "text/plain"),
+        ("Content-Length", str(len(data)))
+    ])
+    return iter([data])
+
