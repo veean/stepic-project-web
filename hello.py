94cdffe57ce7a7057d7e8b9441c8d9857521d816
veean <nersesowm@gmail.com>
2016-11-23 19:34:53 +0000

    all

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 def app(environ, start_response):
     status = "200 OK"
     headers = [("Content-Type", "text/plain")]
-    start_response(status, )
+    start_response(status, headers)
     url_payload =  environ['QUERY_STRING'].split('&')
-    env_dict = [element + "\r\n" for element in url_payload]
+    env_dict = [element +"\r\n" for element in url_payload]
     return env_dict
 
