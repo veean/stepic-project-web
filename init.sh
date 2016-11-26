4c282908282e63004a4da2788845ff3cfa13aa2a
veean <nersesowm@gmail.com>
2016-11-27 02:41:02 +0400

    routing

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
+sudo rm -r /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo rm -r /etc/gunicorn.d/*
+
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/django-conf.py /etc/gunicorn.d/django-conf.py
+
 sudo /etc/init.d/gunicorn restart
